bb1ef6536cdafaab5b6c70f28d80db041542826d
junsa <jun1234755@naver.com>
2023-04-08 22:29:32 +0900

    a

---
+++ src/baekjoon_2566.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class baekjoon_2566 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int arr[][] = new int[9][9];
+        int max;
+        int a = 1;
+        int b = 1;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = s.nextInt();
+            }
+        }
+        max = arr[0][0];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                    a = i + 1;
+                    b = j + 1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.printf("%d %d",a, b);
+    }
+}
---
+++ src/baekjoon_2738.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class baekjoon_2738 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int aa[][] = new int[a][b];
+        int bb[][] = new int[a][b];
+        int result[][] = new int[a][b];
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                aa[i][j] = s.nextInt();
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                bb[i][j] = s.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.printf("%d ", aa[i][j] + bb[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
