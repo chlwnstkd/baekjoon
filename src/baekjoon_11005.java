b5235ad3b543d0993f4108304f929cb56679ec09
junsa <jun1234755@naver.com>
2023-04-13 17:38:48 +0900

    23-04-13

---
+++ src/baekjoon_11005.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class baekjoon_11005 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int b = s.nextInt();
+        List<Character> list = new ArrayList<>();
+        while(n>0) {
+            if(n%b<10)
+                list.add((char)(n%b+'0'));
+            else
+                list.add((char)(n%b-10+'A'));
+            n/=b;
+        }
+        for(int i=list.size()-1; i>=0; i--)
+            System.out.print(list.get(i));
+    }
+
+}
