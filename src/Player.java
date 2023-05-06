4c0ec3ef83c954778cbdce54529d61899531c238
junsa <jun1234755@naver.com>
2023-05-06 13:27:33 +0900

    23-05-05

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Player {
+    enum Status {
+        STAY, HIT
+    }
+    private List<Card> cards;
+    private Status status;
+    Player() {
+        this.cards = new LinkedList<Card>();
+        this.status = Status.HIT;
+    }
+  ...
+}
