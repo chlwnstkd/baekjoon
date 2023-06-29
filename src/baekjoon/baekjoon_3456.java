2649078a1d34a49d55b7873f5e3a4b942a5debb4
junsa <jun1234755@naver.com>
2023-06-29 16:13:28 +0900

    23-06-29

---
+++ src/baekjoon/baekjoon_3456.java
@@ -0,0 +1,25 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon_3456 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tc = sc.nextInt();
+
+        for(int i = 1; i <= tc; i++) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = a ^ b ^ c;
+
+            System.out.println("#" + i + " " + d);
+
+
+        }
+    }
+}
+
+
